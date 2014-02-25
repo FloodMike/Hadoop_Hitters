23d04b366d7b8da036f727097e0feffc151b4461
FloodMike <flood.mike@gmail.com>
2014-02-25 09:40:15 -0600

    Update HadoopMapper.java

--- src/HitterLog/HadoopMapper.java
+++ src/HitterLog/HadoopMapper.java
@@ -1,4 +1,3 @@
-
 /*
  * HadoopMapper.java
  *
@@ -26,102 +25,103 @@ public class HadoopMapper extends MapReduceBase implements Mapper<LongWritable,T
     // The Karmasphere Studio Workflow Log displays logging from Apache Commons Logging, for example:
     // private static final Log LOG = LogFactory.getLog("HitterLog.HadoopMapper");
 
-	private final static int MINIMUM_PLATE_APPEARANCES	=	500;
-	
-	private DoubleWritable	one		=	new DoubleWritable();
-	private Text			stat	=	new Text();
-	
+    private final static int MINIMUM_PLATE_APPEARANCES    =    500;
+    
+    private DoubleWritable one = new DoubleWritable();
+    private Text stat = new Text();
+    
     @Override
     public void map(LongWritable key, Text value, OutputCollector<Text, DoubleWritable> output, Reporter reporter)
             throws IOException {
-        String		line	=	value.toString();
-        String[]	stats	=	line.split("\"");
+            	
+        String line = value.toString();
+        String[] stats = line.split("\"");
         
         if(!stats[1].equalsIgnoreCase("Name")) {
-        	
-        	if(Integer.parseInt(stats[7]) >= MINIMUM_PLATE_APPEARANCES) {
-        		
-        		stat.set("Games");
-        		one.set(Double.parseDouble(stats[5]));
-        		output.collect(stat, one);
-        		stat.clear();
-        		
-        		stat.set("Plate Appearances");
-        		one.set(Double.parseDouble(stats[7]));
-        		output.collect(stat, one);
-        		stat.clear();
-        		
-        		stat.set("Hits");
-        		one.set(Double.parseDouble(stats[9]));
-        		output.collect(stat, one);
-        		stat.clear();
-        		
-        		stat.set("Home Runs");
-        		one.set(Double.parseDouble(stats[11]));
-        		output.collect(stat, one);
-        		stat.clear();
-        		
-        		stat.set("Runs");
-        		one.set(Double.parseDouble(stats[13]));
-        		output.collect(stat, one);
-        		stat.clear();
-        		
-        		stat.set("Runs Batted In");
-        		one.set(Double.parseDouble(stats[15]));
-        		output.collect(stat,one);
-        		stat.clear();
-        		
-        		String[] temp;
-        		
-        		stat.set("Walk Percentage");
-        		temp	=	stats[19].split("%");
-        		
-        		if(!temp[0].isEmpty())
-        			one.set((Double.parseDouble(temp[0]) / 100.0));
-        		else
-        			one.set(0.0);
-        		
-        		output.collect(stat, one);
-        		stat.clear();
-        		
-        		stat.set("Strike Out Percentage");
-        		temp	=	stats[21].split("%");
-        		
-        		if(!temp[0].isEmpty())
-        			one.set((Double.parseDouble(temp[0]) / 100.0));
-        		else
-        			one.set(0.0);
-        		
-        		output.collect(stat, one);
-        		stat.clear();
-        		
-        		stat.set("Isolated Power");
-        		one.set(Double.parseDouble(stats[23]));
-        		output.collect(stat, one);
-        		stat.clear();
-        		
-        		stat.set("Batting Average On Balls In Play (BABIP)");
-        		one.set(Double.parseDouble(stats[25]));
-        		output.collect(stat, one);
-        		stat.clear();
-        		
-        		stat.set("Batting Average");
-        		one.set(Double.parseDouble(stats[27]));
-        		output.collect(stat, one);
-        		stat.clear();
-        		
-        		stat.set("On Base Percentage");
-        		one.set(Double.parseDouble(stats[29]));
-        		output.collect(stat,one);
-        		stat.clear();
-        		
-        		
-        		stat.set("Slugging Percentage");
-        		one.set(Double.parseDouble(stats[31]));
-        		output.collect(stat,one);
-        		stat.clear();
-        		
-        	}
+            
+            if(Integer.parseInt(stats[7]) >= MINIMUM_PLATE_APPEARANCES) {
+                
+                stat.set("Games");
+                one.set(Double.parseDouble(stats[5]));
+                output.collect(stat, one);
+                stat.clear();
+                
+                stat.set("Plate Appearances");
+                one.set(Double.parseDouble(stats[7]));
+                output.collect(stat, one);
+                stat.clear();
+                
+                stat.set("Hits");
+                one.set(Double.parseDouble(stats[9]));
+                output.collect(stat, one);
+                stat.clear();
+                
+                stat.set("Home Runs");
+                one.set(Double.parseDouble(stats[11]));
+                output.collect(stat, one);
+                stat.clear();
+                
+                stat.set("Runs");
+                one.set(Double.parseDouble(stats[13]));
+                output.collect(stat, one);
+                stat.clear();
+                
+                stat.set("Runs Batted In");
+                one.set(Double.parseDouble(stats[15]));
+                output.collect(stat,one);
+                stat.clear();
+                
+                String[] temp;
+                
+                stat.set("Walk Percentage");
+                temp    =    stats[19].split("%");
+                
+                if(!temp[0].isEmpty())
+                    one.set((Double.parseDouble(temp[0]) / 100.0));
+                else
+                    one.set(0.0);
+                
+                output.collect(stat, one);
+                stat.clear();
+                
+                stat.set("Strike Out Percentage");
+                temp    =    stats[21].split("%");
+                
+                if(!temp[0].isEmpty())
+                    one.set((Double.parseDouble(temp[0]) / 100.0));
+                else
+                    one.set(0.0);
+                
+                output.collect(stat, one);
+                stat.clear();
+                
+                stat.set("Isolated Power");
+                one.set(Double.parseDouble(stats[23]));
+                output.collect(stat, one);
+                stat.clear();
+                
+                stat.set("Batting Average On Balls In Play (BABIP)");
+                one.set(Double.parseDouble(stats[25]));
+                output.collect(stat, one);
+                stat.clear();
+                
+                stat.set("Batting Average");
+                one.set(Double.parseDouble(stats[27]));
+                output.collect(stat, one);
+                stat.clear();
+                
+                stat.set("On Base Percentage");
+                one.set(Double.parseDouble(stats[29]));
+                output.collect(stat,one);
+                stat.clear();
+                
+                
+                stat.set("Slugging Percentage");
+                one.set(Double.parseDouble(stats[31]));
+                output.collect(stat,one);
+                stat.clear();
+                
+            }
         }
     }
 }
